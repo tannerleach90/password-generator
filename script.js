f1138a2d534c44b8602b86376848e8e9c1d57365
Tanner Leach <tanner.leach90@gmail.com>
2020-02-06 12:11:20 -0700

    working finally

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var generateBtn = document.querySelector("#generate");
 // var generateBtn = document.getElementById("generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = lowercase.toUpperCase();
-var special = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var numbers = "0123456789"
-// Write password to the #password input
+
 function writePassword() {
   var params = {
     lowercase: confirm("Would you like to use lowercase characters?"),
@@ -24,19 +24,19 @@ function writePassword() {
       ),
     )
   }
-  
 
-        var password = generatePassword(params); 
-        var passwordText = document.querySelector("#password");
 
-        passwordText.value = password;
+  var password = generatePassword(params);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
 
 function generatePassword(options) {
   if (options.uppercase) {
     password += getRandomValue(uppercase);
-    }
+  }
   if (options.lowercase) {
     password += getRandomValue(lowercase);
   }
@@ -46,24 +46,14 @@ function generatePassword(options) {
   if (options.lowercase) {
     password += getRandomValue(numbers);
   }
-return password.slice(0, options.length)
-}
-     
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+  return password.slice(0, options.length)
 
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria/
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+  function getRandomValue(str) {
+    return str[Math.floor(Math.random()) * str.length]
+  }
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
