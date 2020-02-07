c7aa406269e06e7282ccd6c21a7fa3123a92062f
Tanner Leach <tanner.leach90@gmail.com>
2020-02-06 21:56:59 -0700

    script and readme finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password-generator
+
+https://tannerleach90.github.io/password-generator/
+
+##Method: Asking the user via the javascript prompt functions (confirm) what they want the parameters of their password to be. Storing their options that they selected in an object. Then validating that the password both exists and is the right length via params.length property. Because params.length is undefined, it fails a check and that is where the while loop comes in. From my understanding, based on the length and their options, the loop goes over the random generator a certain amount of times. Random generator picks a random character from each line and picks a random character from each user params choices up to the length chosen. My roommate helped me.
--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// 
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 // var generateBtn = document.getElementById("generate");
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppercase = lowercase.toUpperCase();
-var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-var numbers = "0123456789"
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var special = "!#%&'()*+,-./:;<=>?@[]^_`{|}~";
+var numbers = "123456789";
+
+// Write password to the #password input
 function writePassword() {
   var params = {
     lowercase: confirm("Would you like to use lowercase characters?"),
     uppercase: confirm("Would you like to use uppercase characters?"),
     specialChars: confirm("Would you like to use special characters?"),
-    numericChars: confirm("Would you like to use numeric characters?")
+    numericChars: confirm("Would you like to use numeric characters?"),
   };
+
+  debugger;
 
   while (
     params.length < 8 ||
@@ -20,11 +26,11 @@ function writePassword() {
     params.length === undefined
   ) {
     params.length = parseInt(
-      prompt("How long would you like your password to be? Greater than 8 but less than 128."
-      ),
-    )
+      prompt(
+        "How long would you like your password to be? Greater than 8 less than 128"
+      )
+    );
   }
-
 
   var password = generatePassword(params);
   var passwordText = document.querySelector("#password");
@@ -32,28 +38,36 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword(options){
+  var password = "";
 
-function generatePassword(options) {
-  if (options.uppercase) {
-    password += getRandomValue(uppercase);
-  }
-  if (options.lowercase) {
-    password += getRandomValue(lowercase);
-  }
-  if (options.specialChars) {
-    password += getRandomValue(special);
-  }
-  if (options.lowercase) {
-    password += getRandomValue(numbers);
-  }
-  return password.slice(0, options.length)
+  for (let i =0; i < options.length; i++) {
+    if (options.uppercase) {
+      password += getRandomValue(uppercase);
+    }
+    if (options.lowercase) {
+      password += getRandomValue(lowercase);
+    }
+    if (options.specialChars) {
+      password += getRandomValue(special);
+    }
+    if (options.numericChars) {
+      password += getRandomValue(numbers);
+    }
 
-  function getRandomValue(str) {
-    return str[Math.floor(Math.random()) * str.length]
   }
 
+ 
+  
+
+  return password.slice(0, options.length);
 }
-
+  
+  function getRandomValue(str) {
+    console.log("random", Math.floor(Math.random() * str.length))
+    console.log("str", str)
+    return str[Math.floor(Math.random() * str.length)];
+  }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+  
